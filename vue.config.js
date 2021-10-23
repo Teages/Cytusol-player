1709bdfe34db80c52e441b4f5630a1a80d3135da
Teages <Teages@teages.xyz>
2021-10-24 00:21:48 +0800

    PWA test

--- vue.config.js
+++ vue.config.js
@@ -26,5 +26,12 @@ module.exports = {
     themeColor: '#329EF4',
     msTileColor: '#000000',
     appleMobileWebAppCapable: 'yes',
+    iconPaths: {
+      favicon32: 'img/icons/favicon-32x32.png',
+      favicon16: 'img/icons/favicon-16x16.png',
+      appleTouchIcon: 'img/icons/apple-touch-icon.png',
+      maskIcon: 'img/icons/safari-pinned-tab.svg',
+      msTileImage: 'img/icons/msapplication-icon-192x192.png'
+    }
   }
 }
