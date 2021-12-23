a6347b9addaee6bd64233573a985ebb1d5ead4e6
Teages <Teages@teages.xyz>
2021-12-23 15:46:42 +0800

    using local mdi fonts

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,7 +1,13 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify/lib/framework';
 
+import 'typeface-roboto/index.css'
+import '@mdi/font/css/materialdesignicons.min.css'
+
 Vue.use(Vuetify);
 
 export default new Vuetify({
+  icons: {
+    iconfont: 'mdi'
+  }
 });
